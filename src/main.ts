b658df8798fa1f3fbcd080e2f73a783b3d1a107f
tiago.dasilva <tiago.dasilva@el.com.br>
2024-10-15 16:09:09 -0300

    fix: comentar no arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//Teste do GitHub
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
